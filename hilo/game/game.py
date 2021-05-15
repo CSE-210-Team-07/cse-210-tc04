2adffb213b625c454ffde65957c8cd3ee1174f02
Michael Heston <m45t3rhest@gmail.com>
2021-05-15 13:22:01 -0600

    Fixed errors and added functions

--- hilo/game/game.py
+++ hilo/game/game.py
@@ -1,21 +1,27 @@
+
 class game:
-    def __init__(self):
-        self.newCard = 0
-        self.oldCard = 0
+	def __init__(self):
+		self.newCard = 0
+		self.oldCard = 0
+		self.score = 0
 
-    def keep_playing(score):
-        if score == 0:
-            return False
-        else:
-            return True
+	def keep_playing(self):
+		if self.score <= 0:
+			return False
+		else:
+			return True
 
-    def test_guess(self, guess):
-        if guess == 'h' and self.newCard > self.oldCard:
-            return True
-        else:
-            return False
-
-        if guess == 'l' and self.newCard < self.oldCard:
-            return True
-        else: 
-            return False
+	def test_guess(self, guess):
+		if guess == 'h' and self.newCard > self.oldCard:
+			return True
+		else:
+			return False
+	
+	def add_points(self):
+		self.score += 100
+	
+	def sub_points(self):
+		self.score -= 75
+	
+	def get_points(self):
+		return self.score
